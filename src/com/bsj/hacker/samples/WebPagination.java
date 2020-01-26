af55d0f70e606b5a1eeaab423e19053d018968a6
jisamb5 <muneeswaran.balasubramanianan@jnhunt.com>
2020-01-26 10:29:11 -0600

    Hacker Samples

--- src/com/bsj/hacker/samples/WebPagination.java
+++ src/com/bsj/hacker/samples/WebPagination.java
@@ -1,16 +1,74 @@
 package com.bsj.hacker.samples;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class WebPagination {
 
-    static List<String> nameList=new ArrayList<>();
-    public static List<String> fetchItemsToDisplay(List<List<String>> items, int sortParameter, int sortOrder, int itemPerPage, int pageNumber){
-        if(sortOrder==0){
-            //Collections.sort(items.);
-        }
-        return nameList;
+    public static List<String> fetchItemsToDisplay(List<List<String>> items,
+                                                   int sortParameter, int sortOrder, int itemPerPage, int pageNumber) {
+// Write your code here
+        if(pageNumber > items.size() / itemPerPage || sortParameter > 2 ||
+                sortOrder > 1)
+            return null;
+        items = sortItems(items, sortParameter, sortOrder);
+        List<String> result = new ArrayList<String>();
+        for(int i = 0; i < itemPerPage && ((pageNumber * itemPerPage + i) <
+                items.size()); i++)
+            result.add(items.get(pageNumber * itemPerPage + i).get(0));
+        return result;
     }
+    private static List<List<String>> sortItems(List<List<String>> items, int
+            sortParameter, int sortOrder) {
+        switch(sortParameter){
+            case 0:
+                if(sortOrder == 0)
+                    Collections.sort(items, new Comparator<List<String>>() {
+                        @Override
+                        public int compare(List<String> o1, List<String> o2)
+                        {
+                            return
+                                    o1.get(sortParameter).compareTo(o2.get(sortParameter));
+                        }
+                    });
+                else
+                    Collections.sort(items, new Comparator<List<String>>() {
+                        @Override
+                        public int compare(List<String> o1, List<String> o2)
+                        {
+                            return
+                                    o2.get(sortParameter).compareTo(o1.get(sortParameter));
+                        }
+                    });
+                break;
+            case 1:
+            case 2:
+                if(sortOrder == 0)
+                    Collections.sort(items, new Comparator<List<String>>() {
+                        @Override
+                        public int compare(List<String> o1, List<String> o2)
+                        {
+                            if(Integer.parseInt(o1.get(sortParameter)) >
+                                    Integer.parseInt(o2.get(sortParameter)))
+                                return 1;
+                            return -1;
+                        }
+                    });
+                else
+                    Collections.sort(items, new Comparator<List<String>>() {
+                        @Override
+                        public int compare(List<String> o1, List<String> o2)
+                        {
+                            if(Integer.parseInt(o1.get(sortParameter)) <Integer.parseInt(o2.get(sortParameter)))
+                                return 1;
+                            return -1;
+                        }
+                    });
+                break;
+        }
+        return items;
+    }
+}
 }
